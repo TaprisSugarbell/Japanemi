f216afcea18ca03ccec7b3c5aae484db17d70806
Sugarbell <anonesayu@gmail.com>
2021-06-21 17:56:02 -0600

    Update

--- Japanemi_features/anime_.py
+++ Japanemi_features/anime_.py
@@ -1,8 +1,6 @@
 import re
 import json
-import requests
 import urllib.request
-from bs4 import BeautifulSoup
 from Japanemi_features.extractors import *
 from urllib.parse import quote_plus, unquote
 
