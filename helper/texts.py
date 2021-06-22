0f8ef1bcaf9d2e2e90333647d70c7914aab8a7c1
Sugarbell <anonesayu@gmail.com>
2021-06-21 20:09:27 -0600

    Japanemi v1.4

--- helper/texts.py
+++ helper/texts.py
@@ -2,9 +2,9 @@ import re
 
 
 async def capupload_text(title):
-    title = re.sub(r"[^a-zA-Z0-9_ ]", "", title)
-    cap = "".join(title.split(" ")[-1])
-    twc = "_".join(title.split(" ")[:-1])
+    titl = re.sub(r"[^a-zA-Z0-9_ ]", "", title)
+    cap = "".join(titl.split(" ")[-1])
+    twc = "_".join(titl.split(" ")[:-1])
     twg = " ".join(title.split(" ")[:-1])
     caption =\
         f"#{twc}_{cap}\n" \
