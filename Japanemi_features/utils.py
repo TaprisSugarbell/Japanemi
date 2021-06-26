054b8d786c809ae8ed884ebd9802d338461a0b02
Sugarbell <anonesayu@gmail.com>
2021-06-25 23:50:26 -0600

    Japanemi v4.8 | # FIX thumbs

--- Japanemi_features/utils.py
+++ Japanemi_features/utils.py
@@ -46,8 +46,7 @@ async def place_water_mark(input_file, output_file, water_mark_file):
 
 
 async def take_screen_shot(video_file, output_directory, ttl):
-    out_put_file_name = output_directory + \
-                            "/" + str("thumb") + ".jpg"
+    out_put_file_name = output_directory + str("thumb") + ".jpg"
     # out_put_file_name = output_directory + \
     #                     "/" + str(time.time()) + ".jpg"
     file_genertor_command = [
