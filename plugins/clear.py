ed9644a87f6f86e62443eb31bb85ec327449b8af
Sugarbell <anonesayu@gmail.com>
2021-06-28 18:01:53 -0600

    Japanemi v8.4 | Clear update
    Signed-off-by: Sugarbell <anonesayu@gmail.com>

--- plugins/clear.py
+++ plugins/clear.py
@@ -14,4 +14,8 @@ async def flash(bot, update):
     user = update.from_user.id
     chat_id = update.chat.id
     if user in AUTH_USERS:
-        rmtree(f"./Downloads/{user}")
+        try:
+            rmtree(f"./Downloads/{user}")
+        except Exception as e:
+            print(e)
+        print(os.listdir("Dpwnloads"))
