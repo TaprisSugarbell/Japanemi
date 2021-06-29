0a034e5c8b0a72c5752a329871e4120763382ede
Sugarbell <67601252+taprissugarbell@users.noreply.github.com>
2021-06-28 18:15:14 -0600

    Japanemi v8.5 | New command & clear.py update

--- plugins/watch.py
+++ plugins/watch.py
@@ -9,13 +9,19 @@ AUTH_USERS_STR = os.getenv("AUTH_USERS")
 AUTH_USERS = [int(i) for i in AUTH_USERS_STR.split(" ")]
 
 
-@Client.on_message(filterx.command(["clear"]))
+@Client.on_message(filterx.command(["watch"]))
 async def flash(bot, update):
     user = update.from_user.id
     chat_id = update.chat.id
+    dt = " ".join(update["text"].split(" ")[1:])
     if user in AUTH_USERS:
+        if "rm" in dt:
+            drm = "".join(dt.split("|")[-1])
+            ld = os.listdir(f"./Downloads/{drm}")
         try:
-            rmtree(f"./Downloads/{user}")
+            ld = os.listdir(f"./Downloads/{dt}")
         except Exception as e:
             print(e)
-        print(os.listdir("./Downloads"))
+            ld = os.listdir("./Downloads/")
+        await bot.send_message(chat_id=chat_id,
+                               text=ld)
