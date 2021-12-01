58e88b7d72e598da83a9a184f0f7796f1dedb2c0
Sugarbell <ccruz4344@gmail.com>
2021-12-01 14:55:59 -0600

    Japanemi v14.5 | ...

--- Japanemi/helper/__init__.py
+++ Japanemi/helper/__init__.py
@@ -1,4 +1,5 @@
 import requests
+from .. import sayulog
 from ..AnimeFlash import AnimeFlash, amz
 
 
@@ -15,16 +16,19 @@ def servers(s, episode=None):
         arm_links.append(f'{servers_dict["plus"]}.mp4')
     if "amz" in servers_keys:
         # arm_links.append(amz(a.video(servers_dict["amz"], server="amz")))
-        if episode:
-            arm_links.append(("Amazon", f'amz_{episode}_{servers_dict["amz"]}'))
-        else:
-            r = requests.get(f'https://www.amazon.com/drive/v1/shares/{servers_dict["amz"]}?'
-                             f'shareId={servers_dict["amz"]}&resourceVersion=V2&ContentType=JSON')
-            am = requests.get(
-                f'https://www.amazon.com/drive/v1/nodes/{r.json()["nodeInfo"]["id"]}/children?'
-                f'asset=ALL&limit=1&searchOnFamily=false&tempLink=true&'
-                f'shareId={servers_dict["amz"]}&offset=0&resourceVersion=V2&ContentType=JSON').json()
-            arm_links.append(amz(am))
+        try:
+            if episode:
+                arm_links.append(("Amazon", f'amz_{episode}_{servers_dict["amz"]}'))
+            else:
+                r = requests.get(f'https://www.amazon.com/drive/v1/shares/{servers_dict["amz"]}?'
+                                 f'shareId={servers_dict["amz"]}&resourceVersion=V2&ContentType=JSON')
+                am = requests.get(
+                    f'https://www.amazon.com/drive/v1/nodes/{r.json()["nodeInfo"]["id"]}/children?'
+                    f'asset=ALL&limit=1&searchOnFamily=false&tempLink=true&'
+                    f'shareId={servers_dict["amz"]}&offset=0&resourceVersion=V2&ContentType=JSON').json()
+                arm_links.append(amz(am))
+        except Exception as e:
+            sayulog.error("Amazon error.", exc_info=e)
     if "fembed" in servers_keys:
         if episode:
             arm_links.append(("Fembed", f'fmb_{episode}_{servers_dict["fembed"]}'))
