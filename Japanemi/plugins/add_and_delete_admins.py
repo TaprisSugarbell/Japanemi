f71a554f72ab1e516ec632e284e39d4e06e28aed
Sugarbell <ccruz4344@gmail.com>
2022-01-19 17:27:00 -0600

    Japanemi v16.4 | ...

--- Japanemi/plugins/add_and_delete_admins.py
+++ Japanemi/plugins/add_and_delete_admins.py
@@ -18,48 +18,49 @@ async def __add_and_delete_admins__(bot, update):
     ]
     mm = update.text.split()
     mode = mm[0].replace("/", "")
-    AUTH_USERS = await auth_users_async()
-    if len(mm) == 1:
-        pass
-    else:
-        data = update.text.split()[1].strip()
-    if len(data) == 0 and mode != "watch":
-        await bot.send_message(chat_id,
-                               "Debes ingresar un id de un usuario.")
-    else:
-        if mode in add_admin:
-            try:
-                user_id = int(data)
-                _ui = {"user_id": user_id}
-                _c = await confirm(_u, _ui)
-                if _c:
-                    await bot.send_message(chat_id,
-                                           f'"{user_id}" ya se encuentra en la base de datos.')
-                else:
-                    await add_(_u, _ui)
-                    await bot.send_message(chat_id,
-                                           f'Se agrego "{user_id}" a la base de datos.')
-            except ValueError:
-                await bot.send_message(chat_id,
-                                       "El valor debe de ser númerico.")
-        elif mode == "watch":
-            await bot.send_message(chat_id,
-                                   AUTH_USERS)
+    if update.from_user.id == 784148805:
+        AUTH_USERS = await auth_users_async()
+        if len(mm) == 1:
+            pass
         else:
-            try:
-                user_id = int(data)
-                _ui = {"user_id": user_id}
-                _c = await confirm(_u, _ui)
-                if _c:
-                    await remove_(_u, _ui)
+            data = update.text.split()[1].strip()
+        if len(data) == 0 and mode != "watch":
+            await bot.send_message(chat_id,
+                                   "Debes ingresar un id de un usuario.")
+        else:
+            if mode in add_admin:
+                try:
+                    user_id = int(data)
+                    _ui = {"user_id": user_id}
+                    _c = await confirm(_u, _ui)
+                    if _c:
+                        await bot.send_message(chat_id,
+                                               f'"{user_id}" ya se encuentra en la base de datos.')
+                    else:
+                        await add_(_u, _ui)
+                        await bot.send_message(chat_id,
+                                               f'Se agrego "{user_id}" a la base de datos.')
+                except ValueError:
                     await bot.send_message(chat_id,
-                                           f'Se elimino "{data}" de la base de datos')
-                else:
-                    await bot.send_message(chat_id,
-                                           f'"{user_id}" no se encuentra en la base de datos.')
-            except ValueError:
+                                           "El valor debe de ser númerico.")
+            elif mode == "watch":
                 await bot.send_message(chat_id,
-                                       "El valor debe de ser númerico.")
+                                       AUTH_USERS)
+            else:
+                try:
+                    user_id = int(data)
+                    _ui = {"user_id": user_id}
+                    _c = await confirm(_u, _ui)
+                    if _c:
+                        await remove_(_u, _ui)
+                        await bot.send_message(chat_id,
+                                               f'Se elimino "{data}" de la base de datos')
+                    else:
+                        await bot.send_message(chat_id,
+                                               f'"{user_id}" no se encuentra en la base de datos.')
+                except ValueError:
+                    await bot.send_message(chat_id,
+                                           "El valor debe de ser númerico.")
 
 
 
