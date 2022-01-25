aee3f97a8787de1445800d93aea84e9b6a93c037
Sugarbell <ccruz4344@gmail.com>
2022-01-25 15:00:40 -0600

    Japanemi v19.1 | ...

--- Japanemi/plugins/callback.py
+++ Japanemi/plugins/callback.py
@@ -268,6 +268,7 @@ async def __capjk__(bot, update):
         anime_uri = data_split[1]
         number = data_split[-1]
         url = url_base + anime_uri + "/" + number
+        sayulog.warning(f'{data_split} {url}')
         r = await request_anime_jk(requests, url_base, anime_uri, "/" + number)
         if " " in url:
             url = r.url
